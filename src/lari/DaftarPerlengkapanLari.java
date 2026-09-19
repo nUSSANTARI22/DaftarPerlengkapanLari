b1bd033a3b81cb29704490d475f1fdf298e8928a
nUSSANTARI22 <mutiaranusantari5@gmail.com>
2026-09-19 23:58:47 +0800

    Initial commit

---
+++ src/lari/DaftarPerlengkapanLari.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lari;
+
+/**
+ *
+ * @author nussantari
+ */
+public class DaftarPerlengkapanLari {
+
+    public static void main(String[] args) {
+        String perlengkapan1 = "Sepatu Lari";
+        String perlengkapan2 = "Pakaian Olahraga";
+        String perlengkapan3 = "Botol Minum";
+        String perlengkapan4 = "Jam Tangan";
+
+        String status1 = "Sudah Dibawa";
+        String status2 = "Sudah Dibawa";
+        String status3 = "Sudah Dibawa";
+        String status4 = "Sudah Dibawa";
+
+        System.out.println("Daftar Perlengkapan Lari");
+        System.out.println(perlengkapan1 + ":" + status1);
+        System.out.println(perlengkapan2 + ":" + status2);
+        System.out.println(perlengkapan3 + ":" + status3);
+        System.out.println(perlengkapan4 + ":" + status4);
+    }
+}
